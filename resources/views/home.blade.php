f120fb7bebc737bec4fda6ace5b30b1c0fe25946
RoyPulmones <pulmones@dnsc.edu.ph>
2024-04-11 15:11:20 +0800

    SidePanel Done

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -4,70 +4,111 @@
 <link rel="stylesheet" href="{{ asset('css/admin/dashboard.css')}}">
 <link rel="stylesheet" href="{{ asset('css/global.css')}}">
 @endsection
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css"> <link href='https://unpkg.com/boxicons@2.1.2/css/boxicons.min.css' rel='stylesheet'>
+<!-- Styles  -->
+<link rel="shortcut icon" href="assets/img/kxp_fav.png" type="image/x-icon">
+<link rel="stylesheet" href="assets/css/style.css">
  
-<div class="container-fluid">
-    <!-- ___________________________>SidePanel ____________________________-->
-    <!-- <div class="sidepanel">
-        <div class="container-fluid">
-            <div class="image-container">
-                <a href="/"><img src="{{ asset('images/spc_logo.png') }}" alt="spc-logo"></a>
-                <h2>San Pedro College</h2>
-            </div>
-        </div>
-        <div class="content">  
-            <a href="">Dashboard</a>
-            <a href="">Trasanctions</a>
-            <a href="">Users</a>
-            <a href="">Logout</a>
-        </div> 
-    </div>
-     -->
 
-     <div class="sidepanel">
-        <div class="sidepanel-logo">
-            <a href="/"><img src="{{ asset('images/spc_logo.png') }}" alt="spc-logo"></a>              
-            <h2>San Pedro College</h2>  
-        </div>
-        <div class="sidepanel-content">
-            <div class="item-wraper">
-                <div class="item-logo"></div>
-                <a href="">Dashboard</a>
+
+
+
+
+<div class="sidebar close">
+    <!-- ========== Logo ============  -->
+    <a href="#" class="logo-box">
+      <div> <img src="{{ asset('images/spc_logo.png') }}" alt="spc-logo"></div>
+        <div class="logo-name">San Pedro Colleges</div>
+    </a>
+
+    <!-- ========== List ============  -->
+    <ul class="sidebar-list">
+        <!-- -------- Non Dropdown List Item ------- -->
+        <li>
+            <div class="title">
+                <a href="#" class="link">
+                    <i class='bx bx-grid-alt'></i>
+                    <span class="name">Dashboard</span>
+                </a>
+                <!-- <i class='bx bxs-chevron-down'></i> -->
             </div>
-            <div class="item-wraper">
-                <div class="item-logo"></div>
-                <a href="">Trasanctions</a>
+            <div class="submenu">
+                <a href="#" class="link submenu-title">Dashboard</a>
+                <!-- submenu links here  -->
             </div>
-            <div class="item-wraper">
-                <div class="item-logo"></div>
-                <a href="">Users</a>
+        </li>
+
+        <!-- -------- Dropdown List Item ------- -->
+        <li class="dropdown">
+            <div class="title">
+                <a href="#" class="link">
+                    <i class='bx bx-collection'></i>
+                    <span class="name">Transaction</span>
+                </a>
+                <i class='bx bxs-chevron-down'></i>
             </div>
-        </div>
-        <div class="logout-container">
-            <div class="item-logo"></div>
-            <a href="">Logout</a>
-        </div>
-     </div>
-    <!-- ___________________________>Content ____________________________-->
-    <div class="container">
-        <div>
-            <table class="class">
-                <thead>
-                    <tr>
-                        <td>Time</td>
-                        <td>Job</td>
-                        <td>Transaction No.</td>
-                        <td>Status</td>
-                    </tr>
-                </thead>
-                <tbody>
-                    <tr>
-                    <td>Time</td>
-                    <td>Job</td>
-                    <td>Transaction No.</td>
-                    <td>Active</td>
-                    </tr>
-                </tbody>
-            </table>
-        </div>
-    </div>
+            <div class="submenu">
+                <a href="#" class="link submenu-title">Transaction</a>
+                <a href="#" class="link">Transaction List</a>
+                <a href="#" class="link">Manage Transaction</a>
+            </div>
+        </li>
+
+        <li>
+            <div class="title">
+                <a href="#" class="link">
+                    <i class='bx bxs-log-out'></i>
+                    <span class="name">Logout</span>
+                </a>
+                <!-- <i class='bx bxs-chevron-down'></i> -->
+            </div>
+            <div class="submenu">
+                <a href="#" class="link submenu-title">Logout</a>
+                <!-- submenu links here  -->
+            </div>
+        </li>
+    </ul>
 </div>
+
+<!-- ============= Home Section =============== -->
+<section class="home">
+    <div class="toggle-sidebar">
+        <i class='bx bx-menu'></i>
+        <div class="text">Dashboard</div>
+    </div>
+   
+    </div>
+</section>
+
+
+
+<!-- Link JS -->
+<script >
+    const listItems = document.querySelectorAll(".sidebar-list li");
+
+listItems.forEach((item) => {
+  item.addEventListener("click", () => {
+    let isActive = item.classList.contains("active");
+
+    listItems.forEach((el) => {
+      el.classList.remove("active");
+    });
+
+    if (isActive) item.classList.remove("active");
+    else item.classList.add("active");
+  });
+});
+
+const toggleSidebar = document.querySelector(".toggle-sidebar");
+const logo = document.querySelector(".logo-box");
+const sidebar = document.querySelector(".sidebar");
+
+toggleSidebar.addEventListener("click", () => {
+  sidebar.classList.toggle("close");
+});
+
+logo.addEventListener("click", () => {
+  sidebar.classList.toggle("close");
+});
+
+</script>
