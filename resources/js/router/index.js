4ea0f0545f7cf15f307027c37ac6da084345bcbd
Cymon John Buladaco <cymonbuladaco971@gmail.com>
2024-04-25 07:59:06 +0800

    asasasasd

--- resources/js/router/index.js
+++ resources/js/router/index.js
@@ -5,12 +5,14 @@ import NotFound from '../components/Notfound.vue';
 import AdminDashboard from '../components/Admin/Dashboard.vue';
 import UsersView from '../components/Admin/User/index.vue';
 import UserType from '../components/Admin/UserType/index.vue'
+import Logs from '../components/Admin/Logs/index.vue'
+import Profile from '../components/Admin/Profile/index.vue'
+
+
 //Assesor View
 import AssesorView from '../components/Assesor/index.vue'
 // Cashier View
 import CashierView from '../components/Cashier/index.vue'
-import Logs from '../components/Admin/Logs/index.vue'
-import Profile from '../components/Admin/Profile/index.vue'
 
 //Guard View
 import GuardView from '../components/Guard/index.vue'
